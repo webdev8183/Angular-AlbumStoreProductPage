47cdc9bee687ed9acf95cab9b5c732e395b062d6
Monty Edwards <medwardsfl@comcast.net>
2017-12-06 20:40:25 -0500

    Album and Track Interfaces added
    Added album and track interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
 @Injectable()
 export class ProductService {
 
   private _albumUrl='../assets/album.json';
   constructor(private _http: Http) { };
-  getAlbum(id: number){
+  getAlbum(id: number): Observable <Album>{
     return this._http.get(this._albumUrl).map((response) =>
-    response.json());
+   <Album> response.json());
   }
 }
