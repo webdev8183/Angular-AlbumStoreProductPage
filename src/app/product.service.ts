06d73d9467c986cdeab77380e5e37c8a3596b3fc
Monty Edwards <medwardsfl@comcast.net>
2017-12-06 20:43:50 -0500

    updated album
    updated album again vscode seems to be having trouble with jshint

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,8 +9,8 @@ export class ProductService {
 
   private _albumUrl='../assets/album.json';
   constructor(private _http: Http) { };
-  getAlbum(id: number): Observable <Album>{
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-   <Album> response.json());
+   <Album>response.json());
   }
 }
